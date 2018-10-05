77d74b490d2fec46922a24ec700b6c0f74841b31
Clinton Johnson <dragoon1246224@gmail.com>
2018-10-05 09:39:47 -0400

    Almost done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const HTTPClient = require('./httpClient.js');
-const fs = require('fs');
-const url = 'http://jservice.io/api/category?id=';
+const express = require("express")
+const fs = require("fs");
+const app = express()
+const categories = require ("./categories.json")
+app.use(express.json())
 
-const categoriesArray = [777, 780, 21, 105, 25, 306, 136, 680, 309, 2537, 1470, 18065, 6145, 9646, 15090, 499, 42, 7, 78, 211, 420, 176];
+app.get("/api/category/:id", (req, res) => {
+    console.log('inside app.get')
+    const requestedCategory = categories.find(category => category.id == req.params.id)
+    console.log(requestedCategory)
+    res.send(requestedCategory)
+})
 
-const categoryPromises = categoriesArray.map(id => HTTPClient(url + id))
-
-Promise.all(categoryPromises)
-    .then(categories => 
-        fs.writeFileSync('./categories.json', JSON.stringify(categories))
-    )
+app.listen(3000, ()=> console.log("Yay it works"))
