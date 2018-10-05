8a87ad82d02e12bd4178defc37e00e2a4afb4dce
Clinton Johnson <dragoon1246224@gmail.com>
2018-10-05 10:12:58 -0400

    Done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const fs = require("fs");
 const app = express()
+// const categoriesArray = [777, 780, 21, 105, 25, 306, 136, 680, 309, 2537, 1470, 18065, 6145, 9646, 15090, 499, 42, 7, 78, 211, 420, 176]
 const categories = require ("./categories.json")
 app.use(express.json())
 
